56d8202dce9b0b51ddaf0283e00f12d5256f1214
reem sb <rimssboui@gmail.com>
2023-03-01 12:12:51 +0100

    add service upcoming movies

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private http: HttpClient) { }
+
+  getUpcomingMovies() {
+   return  this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=1f9e4794619064689b9df78dd9e0f6b9')
+  }
+}
